7fed54ac4c1b853f8e2a3218125ddfb570716de4
nigelhaim <nigelsebastian21@gmail.com>
2023-10-19 08:53:36 +0800

    Done Dicee App

--- dicee_app/lib/main.dart
+++ dicee_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:math';
 
+/**
+ * References: 
+ * 
+ * https://docs.flutter.dev/release/breaking-changes/buttons
+ */
 void main() {
   runApp(const MyApp());
 }
@@ -25,20 +29,44 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Row(
       children: <Widget>[
         Expanded(
-            child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Image.asset('images/dice1.png'),
-        )),
+          //FlatButton is now replaced with TextButton
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
+              });
+              print('Left button got pressed');
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          ),
+        ),
         Expanded(
-          child: FlatButton(),
-          child: Image.asset('images/dice1.png'),
+          //FlatButton is now replaced with TextButton
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                rightDiceNumber = Random().nextInt(6) + 1;
+                leftDiceNumber = Random().nextInt(6) + 1;
+              });
+              print('Right button got pressed');
+            },
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
         ),
       ],
     ));
